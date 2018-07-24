e143548473cf7891546023d4a589c0b7a2d36637
lizuoyue <li.zuoyue@foxmail.com>
2018-07-24 17:01:35 +0200

    a

--- Main.py
+++ Main.py
@@ -9,7 +9,27 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from PIL import Image
-import cv2, json
+import cv2, json, socket
+
+def setAvaiGPUs(num_gpus = 1):
+	import subprocess as sp
+	ACC_AVAI_MEM = 10240
+	COMMAND = 'nvidia-smi --query-gpu=memory.free --format=csv'
+	# try:
+	_output_to_list = lambda x: x.decode('ascii').split('\n')[:-1]
+	memory_free_info = _output_to_list(sp.check_output(COMMAND.split()))[1:]
+	memory_free_values = [int(x.split()[0]) for i, x in enumerate(memory_free_info)]
+	avai_gpus = [i for i, x in enumerate(memory_free_values) if x > ACC_AVAI_MEM]
+	if len(avai_gpus) < num_gpus:
+		raise ValueError('Found only %d usable GPUs in the system.' % len(avai_gpus))
+	print(avai_gpus[:num_gpus])
+	os.environ['CUDA_VISIBLE_DEVICES'] = ','.join(map(str, avai_gpus[:num_gpus]))
+	# except Exception as e:
+		# print('\"nvidia-smi\" is probably not installed. GPUs are not masked.', e)
+	return
+
+if socket.gethostname() == 'ait-server-03':
+	setAvaiGPUs()
 
 def find_peaks(heatmap, th, gaussian = False):
 	if gaussian:
